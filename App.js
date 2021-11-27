7c8ec7004f60c1832cf4f0dc4ab4034674c89ac1
Sho Takahashi <sho.takahashi@yts-inc.net>
2021-11-28 00:41:33 +0900

    Change style of the Text component.

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>こんにちは!</Text>
+      <Text style={styles.text}>こんにちは!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +18,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  // 以下を追加
+  text: {
+    fontSize: 40,
+    color: "#ff00ff",
+  }
 });
