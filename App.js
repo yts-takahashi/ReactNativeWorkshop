7de933b7585a595614c5a50f3f1218259158500e
Sho Takahashi <sho.takahashi@yts-inc.net>
2021-11-28 00:58:32 +0900

    Implement the login screen's design.

--- App.js
+++ App.js
@@ -1,26 +1,84 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, Pressable, TextInput, View, Image } from 'react-native';
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>こんにちは!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.main}>
+      <Image source={require("./assets/logo.png")} style={ styles.logo } />
+      <View style={styles.textContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          autoCapitalize="none"
+          autoComplete="email"
+          textContentType="emailAddress"
+          keyboardType="email-address"
+          placeholderTextColor="#777"
+          />
+      </View>
+      <View style={styles.textContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          autoComplete="password"
+          secureTextEntry
+          placeholderTextColor="#777"
+        />
+      </View>
+      <View style={styles.buttonContainer}>
+        <Pressable style={styles.button}>
+          <Text style={styles.buttonText}>Log in</Text>
+        </Pressable>
+      </View>
+      <View style={styles.buttonContainer}>
+        <Pressable style={styles.button}>
+          <Text style={styles.buttonText}>Sign Up</Text>
+        </Pressable>
+      </View>
     </View>
-  );
-}
+)}
 
 const styles = StyleSheet.create({
-  container: {
+  logo: {
+    width: 300,
+    height: 300,
+  },
+  main: {
     flex: 1,
-    backgroundColor: '#fff',
+    height: "100%",
+    backgroundColor: "#c5f0e7",
+    alignItems: "center",
+    justifyContent: "center",
+    padding:32,
+  },
+  textContainer:{
+    width: '100%',
+    marginVertical: 12,
+  },
+  input: {
+    borderColor: "gray",
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 10,
+    backgroundColor: "#eeeeee",
+  },
+  button: {
     alignItems: 'center',
     justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 32,
+    elevation: 3,
+    backgroundColor: 'black',
   },
-  // 以下を追加
-  text: {
-    fontSize: 40,
-    color: "#ff00ff",
+  buttonText: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
+  },
+  buttonContainer: {
+    width: "100%",
+    paddingTop: 16,
   }
 });
